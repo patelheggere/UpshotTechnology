c8d77aa2b0a1e2180adfb31158ea23a7428f70df
Veerendra patel <patelheggere@gmail.com>
2018-11-22 08:57:39 +0530

    update and fetch

---
+++ app/src/main/java/com/example/admin/upshothelloworld/activities/FetchActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.upshothelloworld.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FetchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fetch);
+    }
+}
---
+++ app/src/main/java/com/example/admin/upshothelloworld/activities/ListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.admin.upshothelloworld.activities;
+
+public class ListAdapter {
+}
